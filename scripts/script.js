c4e90f92226722ad89ec7339f14f8e4803da57cf
CaVaAnWl <arcade.studio.jeux@gmail.com>
2024-10-17 17:18:18 +0000

    rectification

--- scripts/script.js
+++ scripts/script.js
@@ -1,50 +1,55 @@
 function send() {
     var textinput = document.getElementById('userInput').value;
     document.getElementById('userInput').value = ''; 
-
-    var chatbox = document.getElementById('chatbox');
-    var messageDiv = document.createElement('div');
-
-    messageDiv.classList.add('message', "user");
-
     if (textinput === "") {
-        textinput = "Veuillez remplire la barre de texte.";
+        stop();
     }
 
-    messageDiv.textContent = textinput;
-
-    chatbox.appendChild(messageDiv);
-
-    chatbox.scrollTop = chatbox.scrollHeight;
-    let vartest = textinput;
-
-    if (vartest === "bonjour" || vartest==="Bonjour") {
-        var reponseDiv = document.createElement('div');
-        reponseDiv.classList.add('message', "user");
-        reponseDiv.textContent = 'Bonjour! Comment puis-je vous aider ?';
-        chatbox.appendChild(reponseDiv);
-    } else if (vartest.includes('ça va')) {
-        var reponseDiv = document.createElement('div');
-        reponseDiv.classList.add('message', "user");
-        reponseDiv.textContent = 'Je suis juste un programme, mais merci de demander !';
-        chatbox.appendChild(reponseDiv);
-    } else if (vartest.includes('aide')) {
-        var reponseDiv = document.createElement('div');
-        reponseDiv.classList.add('message', "user");
-        reponseDiv.textContent = 'Que puis-je faire pour vous ?';
-        chatbox.appendChild(reponseDiv);
-    } else if (vartest.includes('contacter')){
-        var reponseDiv = document.createElement('div');
-        reponseDiv.classList.add('message', "user");
-        reponseDiv.textContent = 'Vous pouvez contacter Arcade Studio via l\'adresse mail arcade.studio@gmail.com.';
-        chatbox.appendChild(reponseDiv);
-    }
     else {
-        var reponseDiv = document.createElement('div');
-        reponseDiv.classList.add('message', "user");
-        reponseDiv.textContent = 'Désolé je ne comprends pas.';
+
+        var chatbox = document.getElementById('chatbox');
+        var messageDiv = document.createElement('div');
+
+        messageDiv.classList.add('message', "user");
+
+        messageDiv.textContent = textinput;
+
+        chatbox.appendChild(messageDiv);
+
+        chatbox.scrollTop = chatbox.scrollHeight;
+        let vartest = textinput;
+
+        if (vartest === "bonjour" || vartest==="Bonjour") {
+            var reponseDiv = document.createElement('div');
+            reponseDiv.classList.add('message', "user");
+            reponseDiv.textContent = 'Bonjour! Comment puis-je vous aider ?';
+            chatbox.appendChild(reponseDiv);
+        } else if (vartest.includes('ça va')) {
+            var reponseDiv = document.createElement('div');
+            reponseDiv.classList.add('message', "user");
+            reponseDiv.textContent = 'Je suis juste un programme, mais merci de demander !';
+            chatbox.appendChild(reponseDiv);
+        } else if (vartest.includes('aide')) {
+            var reponseDiv = document.createElement('div');
+            reponseDiv.classList.add('message', "user");
+            reponseDiv.textContent = 'Que puis-je faire pour vous ?';
+            chatbox.appendChild(reponseDiv);
+        } else if (vartest.includes('contacter')){
+            var reponseDiv = document.createElement('div');
+            reponseDiv.classList.add('message', "user");
+            reponseDiv.textContent = 'Vous pouvez contacter Arcade Studio via l\'adresse mail arcade.studio@gmail.com.';
+            chatbox.appendChild(reponseDiv);
+        }
+        else {
+            var reponseDiv = document.createElement('div');
+            reponseDiv.classList.add('message', "user");
+            reponseDiv.textContent = 'Désolé je ne comprends pas.';
+        }
+        stop();
     }
 }
+
+function stop() {console.log("stop();");}
 
 //gestion du clavier :
 document.addEventListener("keydown",(event) => {
@@ -58,7 +63,7 @@ document.addEventListener("keydown",(event) => {
 		}
 
 		if (touche === "ArrowUp"){
-            document.getElementById('userInput').value = textinput; 
+            document.getElementById('userInput').value = vartest; 
 			console.log(touche);
 		}
 
